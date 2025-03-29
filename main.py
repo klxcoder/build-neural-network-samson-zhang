21274a0c92ca431b870beb7b931c9320055db195
klxcoder <klxcoder@gmail.com>
2025-03-29 21:37:05 +0700

    update_params

--- main.py
+++ main.py
@@ -82,3 +82,10 @@ def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
     dW1 = 1 / m * dZ1.dot(X.T)
     db1 = 1 / m * np.sum(dZ1)
     return dW1, db1, dW2, db2
+
+def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1    
+    W2 = W2 - alpha * dW2  
+    b2 = b2 - alpha * db2    
+    return W1, b1, W2, b2
