f51b2b8eb7fc6484d4b51a62285915b45cf4c53b
klxcoder <klxcoder@gmail.com>
2025-03-29 21:29:30 +0700

    forward_prop

--- main.py
+++ main.py
@@ -55,4 +55,11 @@ def ReLU(Z):
 
 def softmax(Z):
     A = np.exp(Z) / sum(np.exp(Z))
-    return A    
+    return A
+
+def forward_prop(W1, b1, W2, b2, X):
+    Z1 = W1.dot(X) + b1
+    A1 = ReLU(Z1)
+    Z2 = W2.dot(A1) + b2
+    A2 = softmax(Z2)
+    return Z1, A1, Z2, A2    
