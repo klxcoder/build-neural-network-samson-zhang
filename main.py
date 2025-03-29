ad413c4c1b027a57f1211c88a78838cda32a37ec
klxcoder <klxcoder@gmail.com>
2025-03-29 21:37:39 +0700

    get_predictions

--- main.py
+++ main.py
@@ -89,3 +89,6 @@ def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
     W2 = W2 - alpha * dW2  
     b2 = b2 - alpha * db2    
     return W1, b1, W2, b2
+
+def get_predictions(A2):
+    return np.argmax(A2, 0)
