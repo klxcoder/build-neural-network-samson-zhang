0bade98b352f04972ab24338b4f991ef5fdb5100
klxcoder <klxcoder@gmail.com>
2025-03-29 21:07:38 +0700

    extract m and n from data shape

--- main.py
+++ main.py
@@ -8,4 +8,16 @@ data = pd.read_csv('./kaggle/input/digit-recognizer/train.csv')
 
 data = np.array(data)
 
-print(data)
+# print(data)
+
+m, n = data.shape
+
+# print(m) # 42000
+# print(n) # 785
+
+"""
+Question: Why is n 785?
+Answer: In the dataset, each row represents a digit image, and each image is 28x28 pixels (total 784 pixels). Each pixel's value is stored in a separate column. Additionally, there’s one extra column for the label (the digit itself).
+
+So, n = 784 (pixels) + 1 (label) = 785.
+"""
