38659c6c6a7c2ec558beaaf2d021756ae9c0fc0a
klxcoder <klxcoder@gmail.com>
2025-03-29 21:39:39 +0700

    make_predictions

--- main.py
+++ main.py
@@ -110,3 +110,8 @@ def gradient_descent(X, Y, alpha, iterations):
     return W1, b1, W2, b2
 
 W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.10, 500)
+
+def make_predictions(X, W1, b1, W2, b2):
+    _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
+    predictions = get_predictions(A2)
+    return predictions
