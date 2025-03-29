f05bb1bea84b0f868d369bef740577a3b1d0d6dd
klxcoder <klxcoder@gmail.com>
2025-03-29 17:59:58 +0700

    version.py

---
+++ version.py
@@ -0,0 +1,9 @@
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib
+
+print(sys.version) # 3.10.16 (main, Dec 11 2024, 16:24:50) [GCC 11.2.0]
+print(np.__version__) # 2.2.4
+print(pd.__version__) # 2.2.3
+print(matplotlib.__version__) # 3.10.1
