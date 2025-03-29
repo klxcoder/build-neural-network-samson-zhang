facdff3207e5006fb36125f3aee60cdb33584688
klxcoder <klxcoder@gmail.com>
2025-03-29 21:15:22 +0700

    test data and train data

--- main.py
+++ main.py
@@ -23,3 +23,19 @@ So, n = 784 (pixels) + 1 (label) = 785.
 """
 
 np.random.shuffle(data) # shuffle before splitting into dev and training sets
+
+# Test data
+data_dev = data[0:1000].T
+Y_dev = data_dev[0]
+X_dev = data_dev[1:n]
+X_dev = X_dev / 255.
+
+# Train data
+data_train = data[1000:m].T
+Y_train = data_train[0]
+X_train = data_train[1:n]
+X_train = X_train / 255.
+_,m_train = X_train.shape
+
+# print(X_train.shape) # (784, 41000)
+# print(m_train) # 41000
